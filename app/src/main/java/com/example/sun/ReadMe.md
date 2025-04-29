65eb91c3b0e499a4ac5a161b164dae1d491ca2d4
PolkaX <alex06040505@gmail.com>
2025-04-29 14:54:47 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sun/ReadMe.md
@@ -0,0 +1,5 @@
+[Persist data with Room](https://developer.android.com/codelabs/basic-android-kotlin-compose-persisting-data-room#0)
+[Read and update data with Room](https://developer.android.com/codelabs/basic-android-kotlin-compose-update-data-room?hl=en#0)
+
+
+[(Deprecated) Advanced Android in Kotlin 05.1:Testing Basics](https://developer.android.com/codelabs/advanced-android-kotlin-training-testing-basics?index=..%2F..index#0)
---
+++ app/src/main/java/com/example/sun/ui/navigation/InventoryNavGraph.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.sun.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.sun.ui.home.HomeDestination
+import com.example.sun.ui.home.HomeScreen
+import com.example.sun.ui.item.ItemDetailsDestination
+import com.example.sun.ui.item.ItemDetailsScreen
+import com.example.sun.ui.item.ItemEditDestination
+import com.example.sun.ui.item.ItemEditScreen
+import com.example.sun.ui.item.ItemEntryDestination
+import com.example.sun.ui.item.ItemEntryScreen
+
+/**
+ * Provides Navigation graph for the application.
+ */
+/*><>
+Экраны со своими аргументами:
+ HomeScreen
+ ItemEntryScreen
+ ItemDetailsScreen
+ ItemEditScreen
+ */
+@Composable
+fun InventoryNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = HomeDestination.route,
+        modifier = modifier
+    ) {
+        composable(route = HomeDestination.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(ItemEntryDestination.route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${ItemDetailsDestination.route}/${it}")
+                }
+            )
+        }
+
+        composable(route = ItemEntryDestination.route) {
+            ItemEntryScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
+
+        composable(
+            route = ItemDetailsDestination.routeWithArgs,
+            arguments = listOf(navArgument(ItemDetailsDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            ItemDetailsScreen(
+                navigateToEditItem = { navController.navigate("${ItemEditDestination.route}/$it") },
+                navigateBack = { navController.navigateUp() }
+            )
+        }
+
+        composable(
+            route = ItemEditDestination.routeWithArgs,
+            arguments = listOf(
+                navArgument(ItemEditDestination.itemIdArg) { type = NavType.IntType })
+        ) {
+            ItemEditScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
+    }
+}
